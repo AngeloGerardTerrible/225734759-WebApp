ceec909f38e0eb3c18119f672562d6caebb7cb22
AngeloGerardTerrible <ant4759@students.uc-bcf.edu.ph>
2024-01-29 16:11:32 +0800

    Indroduction

---
+++ Introduction/script.js
@@ -0,0 +1,11 @@
+// Simple alert when the page loads
+windows.onload = function () {
+    alert("Welcome to our website!");
+}
+
+//Exaxmple of handling a button click
+function displayMessage(){
+    var message = "Button clicked!";
+    console.log(message);
+    alert(message);
+}
